edf87a10cb623a71fd6aa712ae3dc94169e227af
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-19 10:09:41 -0300

    Layout root + estilização com tailwind

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Cabecalho() {
   return (
-        <header>
-          <nav>
-            <Link href="/produtos/estaticas/tenis">TÊNIS</Link>
-            <Link href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
-            <Link href="/produtos/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
+        <header className="bg-gray-800 text-white h-16">
+          <nav className="flex flex-row justify-around">
+            <Link className="link-cabecalho" href="/produtos/estaticas/tenis">TÊNIS</Link>
+            <Link className="link-cabecalho" href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
+            <Link className="link-cabecalho" href="/produtos/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
           </nav>
         </header>
   )
---
+++ src/components/Rodape/Rodape.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Rodape() {
+  return (
+    <footer className="bg-gray-800 text-white h-16 fixed w-full bottom-0">
+        <div>
+            <p>Desenvolvido por: <span>pf0670</span></p>
+            <p>NEXT.js - React.js - GitHub - tailwindcss</p>
+        </div>
+    </footer>
+  )
+}
